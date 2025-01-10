a2a19a935edf9e5dcdea92817f38ff37f8b56194
abidoyejay@gmail.com <abidoyejay@gmail.com>
2025-01-10 08:59:55 +0100

    install package

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,41 @@
-import { useState } from 'react'
-import './App.css'
+import { createBrowserRouter } from 'react-router-dom';
+import { createRoutesFromElements } from 'react-router-dom';
+import { Route } from 'react-router-dom';
+import { Root } from './Pages/Root';
+import { Signin } from './Pages/Signin';
+import { RouterProvider } from 'react-router-dom';
+import { Home } from './Pages/Home';
+import { Register } from './Pages/Register';
+import { Welcome } from './Pages/Welcome';
+import { UserTypeSelection } from './Pages/UserTypeSelection';
+
+
+
+
+
+
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Root />}>
+
+  
+        <Route index element={<Home />} />
+        <Route path="signin" element={<Signin />} />
+        <Route path="UserTypeSelection" element={<UserTypeSelection />} />
+        
+            
+        
+
+      
+    </Route>
+  )
+);
 
 export function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div></div>
-    )
-  }
+    <RouterProvider router={router} />
+  );
+};
+
+
